8897521716642607d74b61e4ea5b5eed33be9074
Ayushrai83 <gurukul4ayush@gmail.com>
2024-10-20 18:47:40 +0530

    CorsConfig setup

--- backend/src/main/java/com/gurukul/Hotel/service/CustomUserDetailsService.java
+++ backend/src/main/java/com/gurukul/Hotel/service/CustomUserDetailsService.java
@@ -1,4 +1,4 @@
-package com.gurukul.Hotel.security;
+package com.gurukul.Hotel.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
